1ccf17a3ea4f5c18b2d46c33cdd57896d21d7b41
Kim Neunert <k9ert@gmx.de>
2022-06-30 12:01:35 +0200

    fix tests

--- tests/test_spectrum_rpc.py
+++ tests/test_spectrum_rpc.py
@@ -7,14 +7,14 @@ def test_root(caplog,client):
     caplog.set_level(logging.DEBUG, logger="cryptoadvance.spectrum")
     result = client.get("/")
     assert result.status_code == 200
-    assert result.text == "JSONRPC server handles only POST requests"
+    assert result.data == b"JSONRPC server handles only POST requests"
     result = client.post("/",json={})
     assert result.status_code == 200
-    assert json.loads(result.text)["error"]["message"] == "Method not found (None)"
+    assert json.loads(result.data)["error"]["message"] == "Method not found (None)"
 
 def test_getmininginfo(caplog,client):
     caplog.set_level(logging.INFO)
     caplog.set_level(logging.DEBUG, logger="cryptoadvance.spectrum")
     result = client.post("/", json={"method":"getmininginfo"})
     assert result.status_code == 200
-    assert json.loads(result.text)["result"]["blocks"] == 0
+    assert json.loads(result.data)["result"]["blocks"] == 0
