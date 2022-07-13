2bb6ae71aa843c05f7cbbc454b2071b8b8b9a859
Kim Neunert <k9ert@gmx.de>
2022-07-13 16:19:16 +0200

    Restore proper configuration

--- src/cryptoadvance/spectrum/config.py
+++ src/cryptoadvance/spectrum/config.py
@@ -66,13 +66,18 @@ class EmzyElectrumLiteConfig(LiteConfig):
     ELECTRUM_PORT=int(os.environ.get('ELECTRUM_PORT', default='50002'))
     ELECTRUM_USES_SSL=_get_bool_env_var('ELECTRUM_USES_SSL', default="true")
 
+class EmzyElectrumPostgresConfig(PostgresConfig):
+    ELECTRUM_HOST=os.environ.get('ELECTRUM_HOST', default='electrum.emzy.de')
+    ELECTRUM_PORT=int(os.environ.get('ELECTRUM_PORT', default='50002'))
+    ELECTRUM_USES_SSL=_get_bool_env_var('ELECTRUM_USES_SSL', default="true")
+
 # Level 2: Back to the problem-Space.
 # Convention: ProblemConfig where problem is usually one of Test/Production or so
 
 class TestConfig(NigiriLocalElectrumLiteConfig):
     pass
 
-class ProductionConfig(EmzyElectrumLiteConfig):
+class ProductionConfig(EmzyElectrumPostgresConfig):
     ''' Not sure whether we're production ready, though '''
     SECRET_KEY = os.getenv("SECRET_KEY", secrets.token_urlsafe(16))
 
