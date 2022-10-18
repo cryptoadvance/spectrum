b5a1f852225cabd532c8cd8912cbbf29595f8ae0
Kim Neunert <k9ert@gmx.de>
2022-10-18 14:02:31 +0200

    Use data_folder provided by Serice superclass for txs storage

--- src/cryptoadvance/specterext/spectrum/service.py
+++ src/cryptoadvance/specterext/spectrum/service.py
@@ -28,23 +28,8 @@ class SpectrumService(Service):
     sort_priority = 2
 
     def callback_after_serverpy_init_app(self, scheduler: APScheduler):
-        def every5seconds(hello, world="world"):
-            with scheduler.app.app_context():
-                print(f"Called {hello} {world} every5seconds")
-        # Here you can schedule regular jobs. triggers can be one of "interval", "date" or "cron"
-        # Examples:
-        # interval: https://apscheduler.readthedocs.io/en/3.x/modules/triggers/interval.html
-        # scheduler.add_job("every5seconds4", every5seconds, trigger='interval', seconds=5, args=["hello"])
-        
-        # Date: https://apscheduler.readthedocs.io/en/3.x/modules/triggers/date.html
-        # scheduler.add_job("MyId", my_job, trigger='date', run_date=date(2009, 11, 6), args=['text'])
-        
-        # cron: https://apscheduler.readthedocs.io/en/3.x/modules/triggers/cron.html
-        # sched.add_job("anotherID", job_function, trigger='cron', day_of_week='mon-fri', hour=5, minute=30, end_date='2014-05-30')
-        
-        # Maybe you should store the scheduler for later use:
-        self.scheduler = scheduler
-        init_app(app, standalone=False)
+        # This will create app.spectrum
+        init_app(app, datadir=self.data_folder, standalone=False)
         
         
         for node in app.specter.node_manager.nodes.values():
