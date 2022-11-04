623bd39bfde521fb2ce05bc7acae770ab1a507d5
Kim Neunert <k9ert@gmx.de>
2022-11-04 16:51:27 +0100

    FlaskThread with a fallback-mode

--- src/cryptoadvance/spectrum/util.py
+++ src/cryptoadvance/spectrum/util.py
@@ -51,12 +51,22 @@ class FlaskThread(Thread):
         copied from https://stackoverflow.com/questions/39476889/use-flask-current-app-logger-inside-threading '''
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.app = app._get_current_object()
         self.daemon = True
+        try:
+            self.app = app._get_current_object()
+            self.flask_mode = True
+        except RuntimeError as e:
+            if str(e).startswith("Working outside of application context."):
+                self.flask_mode = False
+            
 
     def run(self):
-        logger.debug(f"New thread started {self._target.__name__}")
-        with self.app.app_context():
+        if self.flask_mode:
+            with self.app.app_context():
+                logger.debug(f"starting new FlaskThread: {self._target.__name__}")
+                super().run()
+        else:
+            logger.debug(f"starting new Thread: {self._target.__name__}")
             super().run()
 
 
