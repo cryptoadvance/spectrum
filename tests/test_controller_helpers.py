b90a0250d87c7ba49bba89326a17e1ce00e62bc6
Kim Neunert <k9ert@gmx.de>
2022-12-02 17:19:07 +0100

    fix tests

--- tests/test_controller_helpers.py
+++ tests/test_controller_helpers.py
@@ -4,4 +4,4 @@ def test_evaluate_current_status():
     node_is_running_before_request, success, host_before_request, host_after_request = False, True, False, True
     changed_host, check_port_and_ssl = evaluate_current_status(node_is_running_before_request, success, host_before_request, host_after_request)
     assert changed_host == True
-    assert check_port_and_ssl == True
+    assert check_port_and_ssl == False
