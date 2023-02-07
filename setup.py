64f24395ec2ef462eff9dfa9c17be7ac3f0b28f5
Kim Neunert <k9ert@gmx.de>
2023-02-07 13:10:05 +0100

    update release infra

--- setup.py
+++ setup.py
@@ -13,24 +13,10 @@ requirements = [
 ]
 
 setup(
-    name="cryptoadvance.spectrum",
-    version="0.3.1",
-    license="MIT license",
-    url="https://github.com/cryptoadvance/spectrum",
-    description="Electrum adaptor for Specter-Desktop",
-    long_description="Bitcoin Core like RPC wallet with Electrum server backend.",
-    author="Stepan Snigirev",
-    author_email="snigirev.stepan@gmail.com",
     packages=find_namespace_packages("src", include=["cryptoadvance.*"]),
     package_dir={"": "src"},
     package_data={},
     # take METADATA.in into account, include that stuff as well (static/templates)
     include_package_data=True,
     install_requires=requirements,
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Framework :: Flask",
-    ],
 )
