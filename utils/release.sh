6965933cf7908159543d6a218c5327b4c917eecc
Kim Neunert <k9ert@gmx.de>
2023-02-08 10:51:33 +0100

    release-script improvements

--- utils/release.sh
+++ utils/release.sh
@@ -47,28 +47,24 @@ case $key in
     shift
     shift
     ;;
-    set)
-    SET="yes"
-    shift
-    ;;
-    commit)
-    COMMIT="yes"
-    shift
-    ;;
     tag)
-    TAG="yes"
+    command="tag"
     shift
     ;;
     build)
-    BUILD="yes"
+    command="build"
     shift
     ;;
     publish)
-    PUBLISH="yes"
+    command="publish"
     shift
     ;;
     release)
-    RELEASE="yes"
+    command="release"
+    shift
+    ;;
+    wait_on_master)
+    command="wait_on_master"
     shift
     ;;
     --debug)
@@ -88,7 +84,7 @@ function help() {
     echo "not yet implemented"
 }
 
-function main() {
+function check_basic_git_release_infra() {
     # Sed is used as there can be whitespaces 
     if ! [ "$(git remote -v | grep upstream | grep 'git@github.com:cryptoadvance/spectrum.git' |  wc -l | sed -e 's/\s*//')" = "2" ]; then
         echo "    --> You don't have the correct upstream-remote. You need this to release. Please do this:"
@@ -100,135 +96,94 @@ function main() {
         echo "    --> You don't have a reasonable origin-remote. You need this to release (especially with --dev). Please add one!"
         exit 2
     fi
-    
+}
+
+function check_on_master_branch() {
     current_branch=$(git rev-parse --abbrev-ref HEAD)
     if [ "$current_branch" != "master" ]; then
         echo "You're currently not on the master-branch, exiting"
         exit 2
     fi
-
     echo "    --> Fetching all tags ..."
     git fetch upstream --tags
     echo "    --> git pull upstream master"
     git pull upstream master
+}
 
+function ask_new_version_if_needed() {
     if [[ -z "$new_version" ]]; then
         echo "What should be the new version? Type in please (e.g. v0.9.3 ):"
         read new_version
     fi
-
     if ! [[ $new_version =~ ^v([0-9]+)\.([0-9]+)\.([0-9]+)(-([0-9A-Za-z-]+))?$ ]]; then 
         echo "version $new_version Does not match the pattern!"
         exit 1; 
     fi
+}
 
-    if [[ -n "$RELEASE_NOTES" ]]; then
+function release_notes() {
+    if [ -z $GH_TOKEN ]; then
+        echo "Your github-token is missing. Please export them like:"
+        echo "export GH_TOKEN="
+        exit 2
+    fi
 
+    latest_version=$(git tag -l "v*" | grep -v 'pre' | grep -v 'dev' | sort -V | tail -1)
 
-        if [ -z $GH_TOKEN ]; then
-            echo "Your github-token is missing. Please export them like:"
-            echo "export GH_TOKEN="
-            exit 2
+    echo "    --> The latest version is $latest_version. "
+    if ! ask_yn ; then
+        echo "Ok, then you type in the latest_version:"
+        read latest_version
+        if ! [[ $new_version =~ ^v([0-9]+)\.([0-9]+)\.([0-9]+)(-([0-9A-Za-z-]+))?$ ]]; then 
+            echo "Does not match the pattern!"
+            exit 1; 
         fi
-
-        latest_version=$(git tag -l "v*" | grep -v 'pre' | grep -v 'dev' | sort -V | tail -1)
-
-        echo "    --> The latest version is $latest_version. "
-        if ! ask_yn ; then
-            echo "Ok, then you type in the latest_version:"
-            read latest_version
-            if ! [[ $new_version =~ ^v([0-9]+)\.([0-9]+)\.([0-9]+)(-([0-9A-Za-z-]+))?$ ]]; then 
-                echo "Does not match the pattern!"
-                exit 1; 
-            fi
-        fi
-
-        echo "Here are the release-notes:"
-        echo "--------------------------------------------------"
-        echo "# Release Notes" > docs/new_release_notes.md
-        echo "" >> docs/new_release_notes.md
-        echo "## ${new_version} $(date +'%B %d, %Y')" >> docs/new_release_notes.md
-        docker run registry.gitlab.com/cryptoadvance/specter-desktop/github-changelog:latest --github-token $GH_TOKEN --branch master cryptoadvance spectrum $latest_version | sort >> docs/new_release_notes.md
-        echo "" >> docs/new_release_notes.md
-        
-
-        cat docs/new_release_notes.md
-        echo "--------------------------------------------------"
-
-        cp docs/release-notes.md docs/release-notes.md.orig
-        sed -i -e '1,2d' docs/release-notes.md.orig # Assuming the release-Notes start with # Release Notes\n
-        cat docs/new_release_notes.md docs/release-notes.md.orig >  docs/release-notes.md
-        rm docs/release-notes.md.orig docs/new_release_notes.md
-
-        echo "Please check your new File and modify as you find approriate!"
-        echo "We're waiting here ..."
-        echo "    --> Should we create a PR-branch now? "
-
-        if ! ask_yn ; then
-            echo "break"
-            #git checkout docs/release-notes.md
-            exit 2
-        fi
-
-        echo "    --> Creating branch ${new_version}_release_notes "
-        git checkout -b ${new_version}_release_notes
-        git add docs/release-notes.md
-        git commit -m "adding release_notes for $new_version"
-        git push --set-upstream origin ${new_version}_release_notes
-
-        echo "Now go ahead and make your PR:"
-        echo "https://github.com/cryptoadvance/spectrum/pulls"
-        exit 0
     fi
 
-    if [[ -n "$SET" ]]; then
-        set
+    echo "Here are the release-notes:"
+    echo "--------------------------------------------------"
+    echo "# Release Notes" > docs/new_release_notes.md
+    echo "" >> docs/new_release_notes.md
+    echo "## ${new_version} $(date +'%B %d, %Y')" >> docs/new_release_notes.md
+    docker run registry.gitlab.com/cryptoadvance/specter-desktop/github-changelog:latest --github-token $GH_TOKEN --branch master cryptoadvance spectrum $latest_version | sort >> docs/new_release_notes.md
+    echo "" >> docs/new_release_notes.md
+    
+
+    cat docs/new_release_notes.md
+    echo "--------------------------------------------------"
+
+    cp docs/release-notes.md docs/release-notes.md.orig
+    sed -i -e '1,2d' docs/release-notes.md.orig # Assuming the release-Notes start with # Release Notes\n
+    cat docs/new_release_notes.md docs/release-notes.md.orig >  docs/release-notes.md
+    rm docs/release-notes.md.orig docs/new_release_notes.md
+
+    echo "Please check your new File and modify as you find approriate!"
+    echo "We're waiting here ..."
+    echo "    --> Should we create a PR-branch now? "
+
+    if ! ask_yn ; then
+        echo "break"
+        #git checkout docs/release-notes.md
+        exit 2
     fi
-    if [[ -n "$COMMIT" ]]; then
-        commit
-    fi
-    if [[ -n "$TAG" ]]; then
-        tag
-    fi
-    if [[ -n "$BUILD" ]]; then
-        build
-    fi
-    if [[ -n "$PUBLISH" ]]; then
-        publish
-    fi
-    if [[ -n "$RELEASE" ]]; then
-        release
-    fi
+
+    echo "    --> Creating branch ${new_version}_release_notes "
+    git checkout -b ${new_version}_release_notes
+    git add docs/release-notes.md
+    git commit -m "adding release_notes for $new_version"
+    git push --set-upstream origin ${new_version}_release_notes
+
+    echo "Now go ahead and make your PR:"
+    echo "https://github.com/cryptoadvance/spectrum/pulls"
+    exit 0
+
 
 }
-
-function build() {
-    echo "    --> Building the package for version $new_version"
-    rm -rf dist
-    python3 -m pip install --upgrade build
-    python3 -m build
-    echo "    --> Done"
-}
-
-function publish() {
-    python3 -m pip install --upgrade twine > /dev/null
-    echo "    --> Publishing the package for version $new_version"
-    if [ -z $DEV ]; then
-        python3 -m twine upload dist/*
-    else
-        python3 -m twine upload --repository testpypi dist/*
-    fi
-}
-
-function release() {
-    tag
-    git checkout $new_version
-    build
-    publish
-}
-
 
 function tag() {
+    check_basic_git_release_infra
+    check_on_master_branch
+    ask_new_version_if_needed
     echo "    --> Should i now create the tag and push the version $new_version ?"
     if [ -n "$DEV" ]; then
         echo "    --> This will push to your origin-remote!"
@@ -248,6 +203,50 @@ function tag() {
         git push upstream $new_version
     fi
 }
+
+function build() {
+    ask_new_version_if_needed
+    echo "    --> Building the package for version $new_version"
+    current_branch=$(git rev-parse --abbrev-ref HEAD)
+    current_tag=$(git describe --tags)
+    if [ "$current_tag" != "$new_version" ]; then
+        echo "You're currently not on the tag $new_version but on $current_branch"
+        echo "Maybe you haven't created the tag, yet. Consider:"
+        echo "./utils/release.sh --new-version $new_version tag"
+        exit 2
+    fi
+    rm -rf dist
+    python3 -m pip install --upgrade build
+    python3 -m build
+    echo "    --> Finsihed building. _version.py:"
+    cat src/cryptoadvance/spectrum/_version.py
+    echo "    --> Done"
+    
+}
+
+function publish() {
+    python3 -m pip install --upgrade twine > /dev/null
+    echo "    --> Publishing the package for version $new_version"
+    if [ -z $DEV ]; then
+        python3 -m twine upload dist/cryptoadvance.spectrum-${new_version}*
+    else
+        python3 -m twine upload --repository testpypi dist/cryptoadvance.spectrum-${new_version}*
+    fi
+}
+
+function release() {
+    echo "    --> We'll do now the tag"
+    tag
+    echo "    --> git checkout $new_version"
+    git checkout $new_version
+    echo "    --> Creating the build"
+    build
+    echo "    --> Publish to pypi"
+    publish
+}
+
+
+
 
 function wait_on_master() {
     echo "# check status of masterbranch ..."
